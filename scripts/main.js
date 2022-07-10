62092cc32e973f70b17ce502fb7c509154cade1b
skhanal5 <skhanal5@terpmail.umd.edu>
2022-07-10 16:06:37 -0500

    Added intro bg, active-link styling

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,18 @@
-/* Sticky Navigation Bar behavior defined */
-window.addEventListener('scroll', (e)=> {
-    const nav = document.querySelector('.navbar');
-    if(window.pageYOffset>30){
-        nav.classList.add("floatingNav");
-    } else {
-        nav.classList.remove("floatingNav");
+const sections = document.querySelectorAll("section");
+const navLi = document.querySelectorAll("navbar floatingNav nav ul li");
+window.onscroll = () => {
+  var current = "";
+
+  sections.forEach((section) => {
+    const sectionTop = section.offsetTop;
+    if (scrollY >= sectionTop - 30) {
+      current = section.getAttribute("id"); }
+  });
+
+  navLi.forEach((li) => {
+    li.classList.remove("active");
+    if (li.classList.contains(current + "link")) {
+      li.classList.add("active");
     }
-});
+  });
+};
