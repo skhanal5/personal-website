09db8f1fac58e6989c5ba2b694d165575dd93d6f
skhanal5 <skhanal5@terpmail.umd.edu>
2022-07-10 21:44:38 -0500

    Partially added swiperjs and experience section

--- scripts/main.js
+++ scripts/main.js
@@ -16,3 +16,14 @@ window.onscroll = () => {
     }
   });
 };
+
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  direction: 'vertical',
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+});
