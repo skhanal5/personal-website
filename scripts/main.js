b09f97dcccfa58894c53617c5f55dbc3cf7abb2b
skhanal5 <skhanal5@terpmail.umd.edu>
2022-07-11 22:18:27 -0500

    Added pagination and updated intro

--- scripts/main.js
+++ scripts/main.js
@@ -19,11 +19,13 @@ window.onscroll = () => {
 
 const swiper = new Swiper('.swiper', {
   // Optional parameters
-  direction: 'vertical',
+  direction: 'horizontal',
   loop: true,
 
   // If we need pagination
   pagination: {
     el: '.swiper-pagination',
+    clickable: 'true',
+    paginationType: "custom",
   },
 });
